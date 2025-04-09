568701da69e0441ef0090b8e4d8fa4df1dffffb8
Brian Schroedl <bschroedl@proton.me>
2025-04-08 20:18:42 -0500

    Remove development features

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Navigation from "./layouts/Navigation.jsx";
+// import Navigation from "./layouts/Navigation.jsx";
 import NameDrawing from "./components/NameDrawing.js";
 
 function App() {
