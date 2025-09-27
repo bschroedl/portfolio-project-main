5413de814ba12405782a3d7d5539d790ce936002
Brian Schroedl <bschroedl@proton.me>
2025-09-26 19:26:23 -0500

    Update App.jsx to implement search and projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 // import { useState } from 'react'
-import NameDrawingDiv from "./components/NameDrawingDiv.jsx";
 import NavBar from "./layouts/Navigation.jsx";
 import SplashScreen from "./components/NameAnimation.jsx";
+import Search from "./components/Search.jsx";
+import Projects from "./components/Projects.jsx";
 
 function App() {
 
-  return (
-    <>
-        <SplashScreen />
-        {/*<NameDrawingDiv />*/}
-        {/* particle network? */}
-        {/*<NavBar />*/}
-        {/*<div className="h-100"></div>*/}
-        {/*<div className="h-100"></div>*/}
-        {/*  Bottom of the page Contact  */}
+    const projectList = [
+        {id: 1, title: 'One', description: 'One time'},
+        {id: 2, title: 'Two', description: 'Twice the x'},
+        {id: 3, title: 'Three', description: 'Triplets'},
+    ]
 
-    </>
+  return (
+    <div className="border-1 border-blue-500">
+        <SplashScreen />
+        <NavBar />
+        <Search />
+        <div>
+            <h1>Projects</h1>
+            {projectList.map((project, index) => (
+                <Projects key={project.id}
+                          title={project.title}
+                          description={project.description}
+                          reverse={index % 2 === 1}/>
+            ))}
+        </div>
+        <div className="border-1 border-orange-500">Bottom</div>
+          {/*Bottom of the page Contact  */}
+    </div>
   )
 }
 
