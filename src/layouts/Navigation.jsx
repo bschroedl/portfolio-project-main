e2d333ebb750eb05c9d594bd4347920da14f680a
Brian Schroedl <bschroedl@proton.me>
2025-09-26 19:24:51 -0500

    Update Navigation.jsx to be closer to new style

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -1,17 +1,14 @@
+import React from "react";
+
 function NavBar() {
     return (
-        <>
-            <nav className="fixed w-1/2 left-1/2 transform -translate-x-1/2
-            top-8 p-96">
-                <ul className="flex flex-row justify-between">
-                    <a href="#" className="bg-[#E0E0E0] rounded-full hover:bg-amber-200">Home</a>
-                    <a href="#" className="bg-[#E0E0E0] rounded-full">About Me</a>
-                    <a href="#" className="bg-[#E0E0E0] rounded-full">Projects</a>
-                    <a href="#" className="bg-[#E0E0E0] rounded-full">Contact</a>
-                    <a href="#" className="bg-[#E0E0E0] rounded-full">GitHub</a>
-                </ul>
-            </nav>
-        </>
+        <nav className="border-1 border-red-500">
+            <ul>
+                <a href="#">Home</a>
+                <a href="#">GitHub</a>
+                <a href="#">Resume</a>
+            </ul>
+        </nav>
     );
 }
 
