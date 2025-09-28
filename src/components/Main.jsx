6b7183412c76dfa13fcceec70b5a700c95a0b6f2
Brian Schroedl <bschroedl@proton.me>
2025-09-27 23:04:36 -0500

    Add Main.jsx for text at top and search eventually

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import Search from "./Search.jsx";
+
+
+function Main() {
+    return (
+        <div className='mt-36 mb-48 w-3/4 mx-auto'>
+            <h1 className='text-center text-8xl text-[#1098F7]'>Brian's Portfolio</h1>
+            <p className='mt-2 w-5/6 mx-auto'>This is a portfolio website to show off what projects I have done and what I can do with react and tailwind css.
+            This is just more filler text to see how much and what size it should be. Maybe I need more or less but we will see.</p>
+            <Search />
+        </div>
+    );
+}
+
+export default Main;
