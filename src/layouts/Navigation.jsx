6e32041b395f63cd8532a451ffc0e67eddb1bf30
Brian Schroedl <bschroedl@proton.me>
2025-09-27 20:31:22 -0500

    Update Navigation.jsx to link GitHub and LinkedIn with logos

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React from 'react';
+
+/*
+Ideas
+- Add animation to image changing so not as hard to go from right to left
+- Add a button to go to the top or back button
+ */
 
 function NavBar() {
     return (
-        <nav className="border-1 border-red-500">
-            <ul>
-                <a href="#">Home</a>
-                <a href="#">GitHub</a>
-                <a href="#">Resume</a>
+        <nav className='fixed top-[10px] left-[10px]'>
+            <ul className='flex flex-row gap-3'>
+                {/* Must be at minimum 21px tall via linked in requirements */}
+                <a href='https://github.com/bschroedl' target='blank'>
+                    <img
+                        className='h-[21px] hover:h-[21px]'
+                        onMouseOver={e => (e.currentTarget.src = 'src/assets/GitHub_Lockup_Dark.png')}
+                        onMouseOut={e => (e.currentTarget.src = 'src/assets/GitHub_Invertocat_dark.png')}
+                        src='src/assets/GitHub_Invertocat_Dark.png' alt='GitHub logo' />
+                </a>
+                <a href='https://www.linkedin.com/in/brian-schroedl' target='blank'>
+                    <img
+                        className='h-[21px] hover:h-[21px]'
+                        onMouseOver={e => (e.currentTarget.src = 'src/assets/LI-Logo.png')}
+                        onMouseOut={e => (e.currentTarget.src = 'src/assets/LI-In-Bug.png')}
+                        src='src/assets/LI-In-Bug.png' alt='LinkedIn logo' />
+                </a>
             </ul>
         </nav>
     );
