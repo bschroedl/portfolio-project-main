6e29baaa0e183055393acb48930d75d459987a4d
Brian Schroedl <bschroedl@proton.me>
2025-09-29 13:35:17 -0500

    Update Main.jsx to not display search component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
             <h1 className='text-center text-8xl text-[#1098F7]'>Brian's Portfolio</h1>
             <p className='mt-2 w-5/6 mx-auto'>This is a portfolio website to show off what projects I have done and what I can do with react and tailwind css.
             This is just more filler text to see how much and what size it should be. Maybe I need more or less but we will see.</p>
-            <Search />
+            {/*<Search />*/}
         </div>
     );
 }
