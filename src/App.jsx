c58dfa9d5371ff604b76890a3bdab9ea1dd640cd
Brian Schroedl <bschroedl@proton.me>
2025-09-29 13:34:09 -0500

    Update App.jsx to only have components inside

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
 // import { useState } from 'react'
 import NavBar from "./layouts/Navigation.jsx";
 import SplashScreen from "./components/NameAnimation.jsx";
-import Projects from "./components/Projects.jsx";
 import Main from "./components/Main.jsx";
+import ProjectList from "./layouts/ProjectList.jsx";
+
+
 
 function App() {
-
-    const projectList = [
-        {id: 1, title: 'One', description: 'One time'},
-        {id: 2, title: 'Two', description: 'Twice the x'},
-        {id: 3, title: 'Three', description: 'Triplets'},
-    ]
-
   return (
-    <div className="border border-blue-500 relative">
+    <div className="relative bg-[#F5F5F5]">
         <SplashScreen />
         <NavBar />
         <Main />
-        <div>
-            {projectList.map((project, index) => (
-                <Projects key={project.id}
-                          title={project.title}
-                          description={project.description}
-                          reverse={index % 2 === 1}/>
-            ))}
-        </div>
-        <div className="border border-orange-500">Bottom</div>
-          {/*Bottom of the page Contact  */}
+        <ProjectList />
     </div>
   )
 }
