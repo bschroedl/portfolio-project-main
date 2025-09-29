beb2abf56220c55f84d78dc8c98a5d24e668bc21
Brian Schroedl <bschroedl@proton.me>
2025-09-29 15:14:44 -0500

    Update Projects.jsx for better responsive design

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 function Projects({title, description, imageLink, gitHubLink, reverse}) {
     return (
-        <div className={'flex ' + (reverse ? 'flex-row-reverse ' : 'flex-row ') + 'h-[80vh] border-t-4 border-[#E0E0E0] rounded-4xl'}>
-            <img src={imageLink} alt={title} className='m-auto max-w-[75vh] max-h-[60vh] rounded-2xl border-r-2 border-b-2 object-contain'/>
+        <div className={(reverse ? 'md:flex md:flex-row-reverse ' : 'md:flex md:flex-row ') + 'h-[80vh] border-t-4 border-[#E0E0E0] rounded-4xl'}>
+            <img src={imageLink} alt={title} className='m-auto md:max-w-[75vh]  md:max-h-[60vh] rounded-2xl border-r-2 border-b-2 object-contain'/>
             <ul className='m-auto w-100'>
                 <li className='text-2xl font-medium text-[#1098F7] '>{title}</li>
                 <li className='ml-4 text-[#1A1A1A]'>{description}</li>
