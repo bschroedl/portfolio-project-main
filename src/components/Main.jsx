a1820db37c298cbe4e022a9f3981a4ece1db60f9
Brian Schroedl <bschroedl@proton.me>
2025-09-29 16:40:17 -0500

    Update Main.jsx for reactive design and correct text

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,13 @@ import Search from "./Search.jsx";
 
 function Main() {
     return (
-        <div className='mt-36 mb-48 w-3/4 mx-auto'>
-            <h1 className='text-center text-8xl text-[#1098F7]'>Brian's Portfolio</h1>
-            <p className='mt-2 w-5/6 mx-auto'>This is a portfolio website to show off what projects I have done and what I can do with react and tailwind css.
-            This is just more filler text to see how much and what size it should be. Maybe I need more or less but we will see.</p>
+        <div className='md:mt-36 md:mb-48 md:w-3/4 mt-18 mb-24 mx-auto'>
+            <h1 className='text-center md:text-8xl text-4xl text-[#1098F7]'>Brian's Portfolio</h1>
+            <p className='mt-2 w-5/6 mx-auto'>
+                Welcome! This portfolio highlights some of the projects I've worked on as a developer.
+                Some of the technologies I've used include Python, Java, HTML, CSS, JavaScript, MySQL, React, Tailwind CSS, and more.
+                Through both school and personal projects, I've learned new languages and improve my skills.
+            </p>
             {/*<Search />*/}
         </div>
     );
